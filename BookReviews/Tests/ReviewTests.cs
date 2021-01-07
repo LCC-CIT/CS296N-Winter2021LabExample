f95927e2d14d1934c320c73359ab37b9a0bcf70d
Brian Bird <birdb@lanecc.edu>
2021-01-07 09:48:02 -0800

    Added validation test

--- BookReviews/Tests/ReviewTests.cs
+++ BookReviews/Tests/ReviewTests.cs
@@ -8,13 +8,21 @@ namespace Tests
 {
     public class ReviewTests
     {
+        FakeReviewRepository fakeRepo;
+        BookController controller;
+
+        // Setup for all tests. Executed for each test.
+        public ReviewTests()
+        {
+            fakeRepo = new FakeReviewRepository();
+            controller = new BookController(fakeRepo);
+        }
+
         [Fact]
         public void AddReviewTest()
         {
             // Arrange
-            var fakeRepo = new FakeReviewRepository();
-            var controller = new BookController(fakeRepo);
-            var review = new Review()
+           var review = new Review()
             {
                 BookTitle = "Grapes of Wrath",
                 AuthorName = "Salinger",
@@ -26,9 +34,31 @@ namespace Tests
             controller.Review(review);
 
             // Assert
-            // Ensure that the review was added to the repository
+            // Ensure that the review was added to the repository with the correct date
             var retrievedReview = fakeRepo.Reviews.ToList()[0];
             Assert.Equal(0, System.DateTime.Now.Date.CompareTo(retrievedReview.ReviewDate.Date));
+        }
+
+        [Fact]
+        public void AddReviewValidationTest()
+        {
+            // Arrange
+            var review = new Review()
+            {
+                BookTitle = "1234567890123456789012345678901234567890" +
+                "1234567890123456789012345678901234567890" +
+                "123456789012345678901",  // too long
+                AuthorName = null,  // required but null
+                Reviewer = new User() { Name = "Me" },
+                ReviewText = ""   // too short
+            };
+
+            // Act
+            controller.Review(review);
+
+            // Assert
+            // Ensure that the review was not added to the repository
+            Assert.Empty(fakeRepo.Reviews.ToList());
         }
     }
 }
