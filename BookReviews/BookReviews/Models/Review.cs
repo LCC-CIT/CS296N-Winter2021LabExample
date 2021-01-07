808dc045d2bd81fb42a16546ad7686d86c8c6257
Brian Bird <birdb@lanecc.edu>
2021-01-07 08:29:09 -0800

    * Program.cs: * User.cs: * Review.cs: * BookReviews.csproj: * Review.cshtml: * BookReviewContextModelSnapshot.cs: * 20201109191750_Initial_SQLServer.cs: * 20201109191750_Initial_SQLServer.Designer.cs: Added validation
    * _ValidationScriptsPartial.cshtml:

--- BookReviews/BookReviews/Models/Review.cs
+++ BookReviews/BookReviews/Models/Review.cs
@@ -1,13 +1,20 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookReviews.Models
 {
     public class Review
     {
         public int ReviewID { get; set; }
+        [Required (ErrorMessage = "Title is required")]
+        [StringLength(100, MinimumLength = 3,
+            ErrorMessage = "Title must be between 3 and 100 characters")]
         public string BookTitle { get; set; }
+        [Required(ErrorMessage = "Author is required")]
+        [StringLength(100, MinimumLength = 3)]
         public string AuthorName { get; set; }
         public User Reviewer { get; set; }
+        [Required]
         public string ReviewText { get; set; }
         public DateTime ReviewDate { get; set; }
     }
--- BookReviews/BookReviews/Models/User.cs
+++ BookReviews/BookReviews/Models/User.cs
@@ -1,9 +1,12 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace BookReviews.Models
 {
     public class User
     {
         public int UserID { get; set; }
+        [StringLength(100, MinimumLength = 3)]
+        [Required]
         public string Name { get; set; }
     }
 }
