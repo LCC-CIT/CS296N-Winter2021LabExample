c83048ad2452853c46b9eb0b2e215cfa0234049c
Brian Bird <ProfBird@users.noreply.github.com>
2021-01-19 20:47:43 -0800

    Remvoed UserID

---
+++ BookReviews/BookReviews/Migrations/20210120044523_NoAppUserID.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookReviews.Migrations
+{
+    public partial class NoAppUserID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserID",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserID",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
